107ab2b5d756b1ffc1a73f02c43697457aea134d
八幡壮 <sou8767@gmail.com>
2019-06-09 22:23:28 +0900

    first commit

---
+++ who-do-i-look-like/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  who-do-i-look-like
+//
+//  Created by 八幡壮 on 2019/06/09.
+//  Copyright © 2019 八幡壮. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
